9037fd33841d55c5d5f8d383dd01036831c070f1
sssong5252 <luckyjude@naver.com>
2023-12-30 13:53:27 +0900

    1230

--- assignment.java
+++ assignment.java
@@ -3,14 +3,17 @@ public class assignment
 {
     public static void main(String[] args) throws IOException
     {
-        int num;
-        num = 1234;
-        System.out.println("num은 " + num + "입니다");
+        int numb;
+        numb = 1234;
+        System.out.println("num은 " + numb + "입니다");
 
         BufferedReader br=
         new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
+        int num = Integer.parseInt(str);
         System.out.println(str);
+        System.out.println("입력받은" + str + "을 정수형" + num + "으로 변환하였습니다");
+
     }
 }
