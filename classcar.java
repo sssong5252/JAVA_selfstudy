8c9f591fd8462540c60930647a9adfc72f272d27
sssong5252 <luckyjude@naver.com>
2023-12-30 14:29:24 +0900

    1230

---
+++ classcar.java
@@ -0,0 +1,16 @@
+class Car {
+    int num;
+    String name;
+}
+
+public class classcar {
+    public static void main(String[] args) {
+        Car car1;
+        car1 = new Car();
+        
+        car1.num = 1023;
+        car1.name = "sonata";
+
+        System.out.println(car1.name);
+    }
+}
