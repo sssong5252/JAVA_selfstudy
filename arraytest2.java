dfa3dbb41eb244f718d8d2149faeb06f1b8fd2fe
sssong5252 <luckyjude@naver.com>
2023-12-31 15:30:04 +0900

    1231

---
+++ arraytest2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+public class arraytest2 {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br=
+        new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        int A = Integer.parseInt(str1);
+
+        double[] B = new double[A];
+
+        for(int i = 0; i < A; i++)
+        {
+            String str2 = br.readLine();
+            double C = Double.parseDouble(str2);
+            B[i] = C;
+        }
+        System.out.println(Arrays.toString(B));
+    }
+}
