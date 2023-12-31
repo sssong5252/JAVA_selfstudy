1ca4be73409fe4a1e0f2d6a6459ad8298a8729e3
sssong5252 <luckyjude@naver.com>
2023-12-31 11:17:02 +0900

    1231

--- arraytest.java
+++ arraytest.java
@@ -1,19 +1,27 @@
 public class arraytest {
+    
     public static void main(String[] args) 
     {
-        char[] hello = new char[5];
-        hello[0] = 'h';
-        hello[1] = 'e';
-        hello[2] = 'l';
-        hello[3] = 'l';
-        hello[4] = 'o';
+        // char[] hello = new char[5];
+        // hello[0] = 'h';
+        // hello[1] = 'e';
+        // hello[2] = 'l';
+        // hello[3] = 'l';
+        // hello[4] = 'o';
 
-        System.out.println(hello[0]);
+        // System.out.println(hello[0]);
 
-        for(int i = 0; i <10; i++)
-        {
-            System.out.println("i는" + i + "이고, 배열은" + hello[i] + "입니다");
-        }
+        // for(int i = 0; i <10; i++)
+        // {
+        //     System.out.println("i는" + i + "이고, 배열은" + hello[i] + "입니다");
+        // }
+        String[] str = new String[5];
+        str[0] = "hello";
+        str[1] = "my";
+        str[2] = "name";
+        str[3] = "is";
+        str[4] = "junyong";
 
+        System.out.println(str[0] + str[1]);
     }
 }
