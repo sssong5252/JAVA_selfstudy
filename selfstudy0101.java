cb9198c4a5beacc933119d68a391d9a82ccd19fa
sssong5252 <luckyjude@naver.com>
2024-01-01 21:11:57 +0900

    0101

---
+++ selfstudy0101.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class selfstudy0101 {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br=
+        new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        int num = Integer.parseInt(str1);
+
+        double[] double_list = new double[num];
+
+        
+    }
+}
