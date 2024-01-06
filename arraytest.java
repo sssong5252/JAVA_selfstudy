72d68a0873f616d486e04cd4e6e773a9618e44fd
sssong5252 <luckyjude@naver.com>
2024-01-06 17:12:44 +0900

    0106

--- arraytest.java
+++ arraytest.java
@@ -1,7 +1,14 @@
+import java.io.*;
+import java.util.*;
+
 public class arraytest {
     
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
+        BufferedReader br=
+        new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<array_list> array_list = new ArrayList<>();
         // char[] hello = new char[5];
         // hello[0] = 'h';
         // hello[1] = 'e';
@@ -21,6 +28,18 @@ public class arraytest {
         str[2] = "name";
         str[3] = "is";
         str[4] = "junyong";
+
+        String str1 = br.readLine();
+        int num = Integer.parseInt(str1);
+
+        double [] array_new = new double[num];
+        for(int i = 0; i < num; i++)
+        {
+            String num2 = br.readLine();
+            int num3 = Integer.parseInt(num2);
+
+            array_new[i] = num3;
+        }
 
         System.out.println(str[0] + str[1]);
     }
