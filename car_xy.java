4af62acf779dd3ee14d859f7bfad65e06927f5e9
sssong5252 <luckyjude@naver.com>
2024-03-22 10:05:20 +0900

    0322

--- car_xy.java
+++ car_xy.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class car_xy {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new scanner();
         
         while(true){
         }
