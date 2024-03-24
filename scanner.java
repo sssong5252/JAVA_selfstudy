b1c99dfa5bcf9a3c9eececc170cdd87852111813
sssong5252 <luckyjude@naver.com>
2024-03-24 16:14:24 +0900

    java swap

--- scanner.java
+++ scanner.java
@@ -1,3 +1,31 @@
+// // import java.util.*;
+
+// // public class scanner {
+
+// //     public static void swap(int[] arr) {
+// //         int tmp = arr[0];
+// //         arr[0] = arr[1];
+// //         arr[1] = tmp;
+// //     }
+
+// //     public static void main(String[] args) {
+// //         scanner = new scanner(System.in);
+// //         int a, b;
+// //         System.out.println("두 정수를 입력하세요: ");
+// //         a = scanner.nextInt();
+// //         b = scanner.nextInt();
+
+// //         System.out.println("스왑 전: a = " + a + ", b = " + b);
+        
+// //         int[] arr = {a, b};
+// //         swap(arr);
+        
+// //         a = arr[0];
+// //         b = arr[1];
+
+// //         System.out.println("스왑 후: a = " + a + ", b = " + b);
+// //     }
+// // }
 // import java.util.*;
 
 // public class scanner {
@@ -9,7 +37,7 @@
 //     }
 
 //     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
+//         scanner = new scanner(System.in);
 //         int a, b;
 //         System.out.println("두 정수를 입력하세요: ");
 //         a = scanner.nextInt();
@@ -26,31 +54,31 @@
 //         System.out.println("스왑 후: a = " + a + ", b = " + b);
 //     }
 // }
-import java.util.*;
-
-public class scanner {
-
-    public static void swap(int[] arr) {
-        int tmp = arr[0];
-        arr[0] = arr[1];
-        arr[1] = tmp;
+public class scanner{
+    int value;
+    
+    public static void main(String[] args){
+        
+        scanner m1 = new scanner(10);
+        scanner m2 = new scanner(20);
+        
+        System.out.println(m1.value + " " + m2.value);
+        
+        swap2(m1,m2);
+        
+        System.out.println(m1.value + " " + m2.value);
+        
     }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a, b;
-        System.out.println("두 정수를 입력하세요: ");
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-
-        System.out.println("스왑 전: a = " + a + ", b = " + b);
+    
+    public scanner(int a) {
+        this.value = a;
+    }
+    
+    private static void swap2(scanner a, scanner b) {
+        int temp;
+        temp = a.value;
+        a.value = b.value;
+        b.value = temp;
         
-        int[] arr = {a, b};
-        swap(arr);
-        
-        a = arr[0];
-        b = arr[1];
-
-        System.out.println("스왑 후: a = " + a + ", b = " + b);
     }
 }
