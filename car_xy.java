5197b74e9897fd5e0bf52a46bda7efe6724847f8
sssong5252 <luckyjude@naver.com>
2024-03-24 15:41:08 +0900

    java swap

--- car_xy.java
+++ car_xy.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class car_xy {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner scanner = new scanner();
+        Scanner scanner = new scanner(System.in);
+
         
         while(true){
         }
--- scanner.java
+++ scanner.java
@@ -1,17 +1,56 @@
+// import java.util.*;
+
+// public class scanner {
+
+//     public static void swap(int[] arr) {
+//         int tmp = arr[0];
+//         arr[0] = arr[1];
+//         arr[1] = tmp;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int a, b;
+//         System.out.println("두 정수를 입력하세요: ");
+//         a = scanner.nextInt();
+//         b = scanner.nextInt();
+
+//         System.out.println("스왑 전: a = " + a + ", b = " + b);
+        
+//         int[] arr = {a, b};
+//         swap(arr);
+        
+//         a = arr[0];
+//         b = arr[1];
+
+//         System.out.println("스왑 후: a = " + a + ", b = " + b);
+//     }
+// }
 import java.util.*;
 
 public class scanner {
+
+    public static void swap(int[] arr) {
+        int tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num1;
-        float num2, sum;
-        String str1;
-        str1 = scanner.nextLine();
-        num1 = scanner.nextInt();
-        num2 = scanner.nextFloat();
+        int a, b;
+        System.out.println("두 정수를 입력하세요: ");
+        a = scanner.nextInt();
+        b = scanner.nextInt();
 
-        sum = num1 + num2;
-        System.out.println(str1);
-        System.out.println(sum);
-    } 
+        System.out.println("스왑 전: a = " + a + ", b = " + b);
+        
+        int[] arr = {a, b};
+        swap(arr);
+        
+        a = arr[0];
+        b = arr[1];
+
+        System.out.println("스왑 후: a = " + a + ", b = " + b);
+    }
 }
