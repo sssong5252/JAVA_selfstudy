1e5652c9aad476c89f00c213640e5629dc31a49f
sssong5252 <luckyjude@naver.com>
2024-05-31 13:12:37 +0900

    pack

--- RandomExam.java
+++ RandomExam.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class RandomExam {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Random ran = new Random();
         int num = ran.nextInt(100);
         System.out.println("0 부터 99 사이의 임의의 값 출력 : " + num);
@@ -9,8 +10,11 @@ public class RandomExam {
         boolean bln = ran.nextBoolean();
         System.out.println("임의의 논리값 출력 : " + bln);
         System.out.println("*** 복권번호 출력 ***");
-        for(int i=0; i<6; i++) {
+        
+        for(int i=0; i<6; i++)
+        {
             num = ran.nextInt(46);
             System.out.println((i+1) +" 번째 복권번호 : " + num);
+        }
     }
-} }
+}
